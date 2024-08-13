e0b4aebf259dcaedc0af355830d3dbfd1a62b31e
Anh.Nguyen <anh.nguyen@aeris.net>
2024-08-13 20:53:48 +0700

    delete images

--- animal_news_frontend/src/components/Footer.jsx
+++ animal_news_frontend/src/components/Footer.jsx
@@ -77,7 +77,7 @@ export const Footer = () => {
             </MDBContainer>
 
             <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-                © 2020 Copyright:
+                © 2024 Copyright:
                 <a className='text-white' href='https://mdbootstrap.com/'>
                     MDBootstrap.com
                 </a>
