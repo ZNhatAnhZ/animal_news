9937072d81576e32c2e2e662f68d421c46a0a4bf
Nhat Anh <ntnhatanh2001@gmail.com>
2025-01-05 20:30:37 +0700

    using localhost for the pod

--- animal_news_frontend/src/config/envVariable.jsx
+++ animal_news_frontend/src/config/envVariable.jsx
@@ -1,3 +1,3 @@
-const backendUrl = import.meta.env.BACKEND_URL ? import.meta.env.BACKEND_URL : 'http://host.docker.internal:8080';
+const backendUrl = import.meta.env.BACKEND_URL ? import.meta.env.BACKEND_URL : 'http://localhost:8080';
 
 export default backendUrl;
